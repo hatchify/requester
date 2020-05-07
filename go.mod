38053ef27c73729497e16714f4b5c69ca0501be2
Classic Massok <marcus@hatchify.co>
2020-05-07 08:59:15 -0400

    cleanup

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/Hatch1fy/requester
 
 go 1.14
-
-require github.com/Hatch1fy/integrations-common v0.1.14
--- opts.go
+++ opts.go
@@ -2,12 +2,10 @@ package requester
 
 import (
 	"net/url"
-
-	common "github.com/Hatch1fy/integrations-common"
 )
 
 // NewOpts will return a new instance of RequestOpts
-func NewOpts(query url.Values, headers common.HeadersMap) (op *Opts) {
+func NewOpts(query url.Values, headers HeadersMap) (op *Opts) {
 	var o Opts
 	o.query = query
 	o.headers = headers
@@ -18,5 +16,5 @@ func NewOpts(query url.Values, headers common.HeadersMap) (op *Opts) {
 // Opts represents optional parameters for an HTTP Request
 type Opts struct {
 	query   url.Values
-	headers common.HeadersMap
+	headers HeadersMap
 }
