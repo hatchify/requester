3f5a7a6358890dc224adee3f7b0545014134abd5
Josh Montoya <josh@montoya.io>
2020-07-21 09:44:44 -0700

    Move interface to it's own file

--- requester.go
+++ requester.go
@@ -7,17 +7,6 @@ import (
 	"net/url"
 )
 
-// Interface needs to implement all needed Requester methods
-type Interface interface {
-	Request(method, path string, body []byte, opts Opts) (resp *http.Response, err error)
-
-	Get(path string, opts ...Opt) (resp *http.Response, err error)
-	Post(path string, body []byte, opts ...Opt) (resp *http.Response, err error)
-	Put(path string, body []byte, opts ...Opt) (resp *http.Response, err error)
-	Patch(path string, body []byte, opts ...Opt) (resp *http.Response, err error)
-	Delete(path string, opts ...Opt) (resp *http.Response, err error)
-}
-
 // New will create a new instance of Requester
 func New(hc *http.Client, baseURL string) (rp *Requester) {
 	var r Requester
